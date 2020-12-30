d6ba9e346dc9309ba4695d72c33fc3a7b66518f2
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-30 15:52:03 -0800

    add required dependencies on server.js

---
+++ server.js
@@ -0,0 +1,7 @@
+// ========== Required Dependencies ========== //
+// Import mysql npm package
+const mysql = require("mysql");
+// Import inquirer npm package
+const inquirer = require("inquirer");
+// Import console.table npm package
+const consoleTable = require("console.table");
