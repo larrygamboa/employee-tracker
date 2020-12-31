9751165d9372053ff7027e0239fe4d69e1b5957c
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-30 16:03:26 -0800

    create mysql connection

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 // Import console.table npm package
 const consoleTable = require("console.table");
+
+// ========== Create credentials for mysql connection ========== //
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  // Your user name
+  user: "root",
+  // Your password
+  password: "l@rG020979!",
+  // Your database name
+  database: "employee_info_db"
+});
+// Create the connection //
+connection.connect(function (err) {
+  // If there is an error, throw it
+  if (err) throw err;
+  // Run a function called startTracker
+  startTracker();
+});
