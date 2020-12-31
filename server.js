88a680498d08e2e50258740e425b582af341baea
Larry Gamboa <techmedia.lg@gmail.com>
2020-12-30 18:31:33 -0800

    create function to start tracker

--- server.js
+++ server.js
@@ -24,3 +24,52 @@ connection.connect(function (err) {
   // Run a function called startTracker
   startTracker();
 });
+
+// Inquirer for CLI control //
+// startTracker function to prompt the user on what to do
+function startTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "Add department",
+        "Add role",
+        "Add employee",
+        "View departments",
+        "View roles",
+        "View employees",
+        "Update employee role",
+        "Quit"
+      ]
+    })
+    .then(function(choice) {
+      console.log("You entered: " + choice.action);
+      switch (choice.action) {
+        case "Add department":
+          addDepartment();
+          break;
+        case "Add role":
+          addRole();
+          break;
+        case "Add employee":
+          addEmployee();
+          break;
+        case "View departments":
+          viewDepartment();
+          break;
+        case "View roles":
+          viewRoles();
+          break;
+        case "View employees":
+          viewEmployees();
+          break;
+        case "Update employee role":
+          updateEmployee();
+          break;
+        default:
+          quit();
+      }
+    });
+}
