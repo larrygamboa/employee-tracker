42895cc613fd4c7065922039fc0e4fc89cbe0df4
Larry Gamboa <techmedia.lg@gmail.com>
2021-01-04 15:52:33 -0800

    update database

--- db/seed.sql
+++ db/seed.sql
@@ -1,18 +1,22 @@
+USE employee_infoDB;
+    
 INSERT INTO department (name)
 VALUES
-	(Management),
-    (Engineering),
-    (Development),
-    (Human Resources),
-    (Accounting);
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
     
 INSERT INTO role (title, salary, department_id)
 VALUES
-	("manager", 125000.00, 1),
-    ("engineer", 100000.00, 2),
-    ("developer", 80000.00, 3),
-    ("human resources", 90000.00, 4),
-    ("accounting", 75000.00, 5);
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
     
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES 
@@ -21,3 +25,15 @@ VALUES
     ("Han", "Solo", 2, NULL),
     ("Luke", "Skywalker", 5, NULL),
     ("Boba", "Fett", 4, NULL);
+    
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('ObiWan', 'Kenobi', 1, NULL),
+    ('Darth', 'Vader', 2, 1),
+    ('Han', 'Solo', 3, NULL),
+    ('Luke', 'Skywalker', 4, 3),
+    ('Boba', 'Fett', 5, NULL),
+    ('Leia', 'Organa', 6, 5),
+    ('Ahsoka', 'Tano', 7, NULL),
+    ('Emperor', 'Palpatine', 8, 7);
